181bc50e89fe147551dfb70dbb28e697a16eee41
Samuele Tondelli <samueletondelli92@gmail.com>
2023-04-04 16:12:29 +0200

    Added files with conversion factors

---
+++ MMR_Hybrid_Display_v2.1/Code/Inc/Data/DataConstantControls.h
@@ -0,0 +1,13 @@
+/*
+ * DataConstantControls.h
+ *
+ *  Created on: 4 apr 2023
+ *      Author: samue
+ */
+
+#ifndef INC_DATA_DATACONSTANTCONTROLS_H_
+#define INC_DATA_DATACONSTANTCONTROLS_H_
+
+
+
+#endif /* INC_DATA_DATACONSTANTCONTROLS_H_ */
