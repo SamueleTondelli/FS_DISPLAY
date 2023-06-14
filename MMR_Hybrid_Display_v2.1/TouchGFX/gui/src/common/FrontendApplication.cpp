1f8c7e3b9752037faa5d3144a9a6bf4649399bd3
Samuele Tondelli <samueletondelli92@gmail.com>
2023-06-14 14:45:37 +0200

    reworked alarm handling with a FSM

--- MMR_Hybrid_Display_v2.1/TouchGFX/gui/src/common/FrontendApplication.cpp
+++ MMR_Hybrid_Display_v2.1/TouchGFX/gui/src/common/FrontendApplication.cpp
@@ -12,6 +12,14 @@ FrontendApplication::FrontendApplication(Model& m, FrontendHeap& heap)
 
 extern uint8_t invalidateCurrentAlarmFlag; //bad, move to somewhere else
 
+enum HANDLEALARM_STATUS {
+	NO_ALARM, //no active alarma
+	ALARM_NOTINT, //alarm in not interruptible
+	ALARM_BONUST, //giving alarm bonus time
+	ALARM_INT, //alarm is interruptible
+	DEACTIVATE_ALARM //deactivation of current alarm, either with flag or timeout
+};
+
 void FrontendApplication::gotoHOMEScreenNoTransition()
 {
 	transitionCallback = touchgfx::Callback<FrontendApplicationBase>(this, &FrontendApplication::gotoHOMEScreenNoTransitionImpl);
@@ -88,94 +96,140 @@ void FrontendApplication::writeAlarmInBuffers(Alarm* alarm, Unicode::UnicodeChar
 	}
 }
 
+//weird way to have a static variable of a class initialized even if there is no instance of the class
 Alarm* FrontendApplication::getCurrentAlarm()
 {
 	static Alarm a = {.contents = NULL};
 	return &a;
 }
 
+/*Alarms works by having a not interruptable time, which unless the button is pressed we show the alarm even if there's an alarm after with a higher priority,
+  after that if there's an alarm with same or lower priority (more important), we switch to that, otherwise we give a bonus time to the current alarm because
+  it's more important
+*/
 void FrontendApplication::handleAlarms(touchgfx::Container* ctAlarm, Unicode::UnicodeChar* nameBuffer, uint16_t nameBufferSize, Unicode::UnicodeChar* valueBuffer, uint16_t valueBufferSize, BoxWithBorder* bxAlarm)
 {
 	Alarm* currentAlarm = FrontendApplication::getCurrentAlarm();
 	static uint32_t currentAlarmStartTime;
-	ctAlarm->setVisible(true);
 
-	//if it's first time we enter here, we initialize currentAlarm and get the timestamp at which the alarm starts
-	if (currentAlarm->contents == NULL)
-	{
-		*currentAlarm = extractAlarmFromQueue();
-		currentAlarmStartTime = uwTick;
-	}
+	static HANDLEALARM_STATUS status = NO_ALARM;
 
-	/*Alarms works by having a not interruptable time, which unless the button is pressed we show the alarm even if there's an alarm after with a higher priority,
-	  after that if there's an alarm with same or lower priority (more important), we switch to that, otherwise we give a bonus time to the current alarm because
-	  it's more important
-	*/
-	//TODO: improve
-	if ((invalidateCurrentAlarmFlag == 1) || (uwTick - currentAlarmStartTime >= ALARM_AUTOMATIC_TIMEOUT_TIME))
+	switch (status)
 	{
-		deactivateAlarm(currentAlarm);
-		invalidateCurrentAlarmFlag = 0;
-	}
-	else
-	{
-		if (uwTick - currentAlarmStartTime < ALARM_NOT_INTERRUPTABLE_TIME)
+	case NO_ALARM: //no current alarm, check if there are any in queue
+		ctAlarm->setVisible(false);
+		if (peekAlarmFromQueue() != NULL) //check if there is an alarm in queue
 		{
-			if ((getAlarmStatus(currentAlarm) == ON) || currentAlarm->priority == NOTIFICATION_PRIORITY) //if the alarm has lowest priority, it means it's a notification, therefore we need to show it for the whole non interruptible time
+			*currentAlarm = extractAlarmFromQueue();
+			currentAlarmStartTime = uwTick;
+			status = ALARM_NOTINT;
+		}
+		break;
+
+	case ALARM_NOTINT: //current alarm is showed even if there is a more important alarm after
+		if (invalidateCurrentAlarmFlag == 1) //check if we need to disable current alarm
+		{
+			status = DEACTIVATE_ALARM;
+		}
+		else if (uwTick - currentAlarmStartTime < ALARM_NOT_INTERRUPTABLE_TIME) //associated amount of time not passed yet, we switch to NO_ALARM only if the current alarm is not on
+		{
+			if (getAlarmStatus(currentAlarm) == ON || currentAlarm->priority == NOTIFICATION_PRIORITY)
 			{
+				ctAlarm->setVisible(true);
 				FrontendApplication::writeAlarmInBuffers(currentAlarm, nameBuffer, nameBufferSize, valueBuffer, valueBufferSize, bxAlarm);
 			}
 			else
 			{
-				ctAlarm->setVisible(false);
-				currentAlarm->contents = NULL;
+				if (currentAlarm->type == DATA) //we remove current alarm from queue
+				{
+					((Data*)currentAlarm->contents)->alarmStatus = OFF_QUEUE;
+				}
+				status = NO_ALARM;
 			}
 		}
-		else
+		else //check which status we need to switch to
 		{
 			Alarm* nextAlarm = peekAlarmFromQueue();
-			if (nextAlarm != NULL)
+			if (nextAlarm == NULL) //no alarm after, so current alarm can be interrupted by any new alarm
 			{
-				if (nextAlarm->priority <= currentAlarm->priority || (uwTick - currentAlarmStartTime >= ALARM_NOT_INTERRUPTABLE_TIME + ALARM_BONUS_TIME))
-				{
-					if (currentAlarm->type == DATA)
-					{
-						Data* data = (Data*)currentAlarm->contents;
-						data->alarmStatus = OFF_QUEUE; //so we can insert again the alarm in the queue
-					}
-					else
-					{
-						//telemetry
-					}
-					//we invalidate current alarm
-					currentAlarm->contents = NULL;
-				}
-				else
-				{
-					if (getAlarmStatus(currentAlarm) == ON)
-					{
-						FrontendApplication::writeAlarmInBuffers(currentAlarm, nameBuffer, nameBufferSize, valueBuffer, valueBufferSize, bxAlarm);
-					}
-					else
-					{
-						ctAlarm->setVisible(false);
-						currentAlarm->contents = NULL;
-					}
-				}
+				status = ALARM_INT;
 			}
 			else
 			{
-				if (getAlarmStatus(currentAlarm) == ON)
+				if (nextAlarm->priority <= currentAlarm->priority)
 				{
-					FrontendApplication::writeAlarmInBuffers(currentAlarm, nameBuffer, nameBufferSize, valueBuffer, valueBufferSize, bxAlarm);
+					if (currentAlarm->type == DATA) //we remove current alarm from queue
+					{
+						((Data*)currentAlarm->contents)->alarmStatus = OFF_QUEUE;
+					}
+					status = NO_ALARM; //next alarm is more important, we switch to NO_ALARM so that we can extract from queue the next alarm
 				}
 				else
 				{
-					ctAlarm->setVisible(false);
-					currentAlarm->contents = NULL;
+					status = ALARM_BONUST; //next alarm is less important, we give current alarm bonus time
 				}
 			}
 		}
+		break;
+
+	case ALARM_BONUST: //there is an alarm after this which is less important
+		if (invalidateCurrentAlarmFlag == 1) //check if we need to disable current alarm
+		{
+			status = DEACTIVATE_ALARM;
+		}
+		else if(uwTick - currentAlarmStartTime < ALARM_NOT_INTERRUPTABLE_TIME + ALARM_BONUS_TIME) //associated amount of time not passed yet, we switch to NO_ALARM only if the current alarm is not on
+		{
+			if (getAlarmStatus(currentAlarm) == ON)
+			{
+				ctAlarm->setVisible(true);
+				FrontendApplication::writeAlarmInBuffers(currentAlarm, nameBuffer, nameBufferSize, valueBuffer, valueBufferSize, bxAlarm);
+			}
+			else
+			{
+				if (currentAlarm->type == DATA) //we remove current alarm from queue
+				{
+					((Data*)currentAlarm->contents)->alarmStatus = OFF_QUEUE;
+				}
+				status = NO_ALARM;
+			}
+		}
+		else //bonus time has passed, we switch to NO_ALARM so that we can go to the next alarm (we know there is one because we end here only if there's an alarm less important after)
+		{
+			status = NO_ALARM;
+		}
+		break;
+
+	case ALARM_INT: //current alarm can be interrupted by any alarm if there is one
+		if (invalidateCurrentAlarmFlag == 1 || (uwTick - currentAlarmStartTime >= ALARM_AUTOMATIC_TIMEOUT_TIME)) //check if we need to disable current alarm (also in case of timeout)
+		{
+			status = DEACTIVATE_ALARM;
+		}
+		else // we switch to NO_ALARM only if the current alarm is not on
+		{
+			if (getAlarmStatus(currentAlarm) == ON)
+			{
+				ctAlarm->setVisible(true);
+				FrontendApplication::writeAlarmInBuffers(currentAlarm, nameBuffer, nameBufferSize, valueBuffer, valueBufferSize, bxAlarm);
+			}
+			else
+			{
+				if (currentAlarm->type == DATA) //we remove current alarm from queue
+				{
+					((Data*)currentAlarm->contents)->alarmStatus = OFF_QUEUE;
+				}
+				status = NO_ALARM;
+			}
+		}
+		break;
+
+	case DEACTIVATE_ALARM: //deactivate current alarm
+		deactivateAlarm(currentAlarm); //wither by the global flag (set by associated button) or timeout
+		invalidateCurrentAlarmFlag = 0;
+		status = NO_ALARM;
+		break;
+
+	default: //we should never get here, something bad happened
+		break;
 	}
 
 	ctAlarm->invalidate();
