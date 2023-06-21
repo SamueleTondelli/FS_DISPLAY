5b0340cbcfa201b8dc557b9db63fdf99e24f97ab
Samuele Tondelli <samueletondelli92@gmail.com>
2023-06-21 11:25:09 +0200

    removed useless flag

--- MMR_Hybrid_Display_v2.1/Core/Src/main.c
+++ MMR_Hybrid_Display_v2.1/Core/Src/main.c
@@ -90,7 +90,6 @@ extern TIM_HandleTypeDef htim5;
 Dataset ds;
 Alarm queue[PRIORITIES][ALARMS_PER_PRIORITY];
 int queueSize[PRIORITIES] = { 0 };
-uint8_t invalidateCurrentAlarmFlag = 0;
 
 TimersList timList;
 
